0b954af2d3063439e6d1509a94b482f32b67644f
Sumit240803 <goyalsumit651@gmail.com>
2025-03-31 21:57:42 +0530

    Day 10

---
+++ Sumit_Goyal_Day10_HandsOn_QuadB/crud/src/components/crud/crud-data-access.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import { getCrudProgram, getCrudProgramId } from '@project/anchor'
+import { useConnection } from '@solana/wallet-adapter-react'
+import { Cluster, Keypair, PublicKey } from '@solana/web3.js'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import toast from 'react-hot-toast'
+import { useCluster } from '../cluster/cluster-data-access'
+import { useAnchorProvider } from '../solana/solana-provider'
+import { useTransactionToast } from '../ui/ui-layout'
+import { access } from 'fs'
+import { error } from 'console'
+interface CreateEntryArgs{
+  title : string;
+  message : string;
+  owner : PublicKey
+}
+export function useCrudProgram() {
+  const { connection } = useConnection()
+  const { cluster } = useCluster()
+  const transactionToast = useTransactionToast()
+  const provider = useAnchorProvider()
+  const programId = useMemo(() => getCrudProgramId(cluster.network as Cluster), [cluster])
+  const program = useMemo(() => getCrudProgram(provider, programId), [provider, programId])
+
+  const accounts = useQuery({
+    queryKey: ['crud', 'all', { cluster }],
+    queryFn: () => program.account.journalEntryState.all(),
+  })
+
+  const getProgramAccount = useQuery({
+    queryKey: ['get-program-account', { cluster }],
+    queryFn: () => connection.getParsedAccountInfo(programId),
+  })
+  const createEntry = useMutation<string,Error,CreateEntryArgs>({
+    mutationKey : [`journalEntry` , `create` , {cluster}],
+    mutationFn :async({title , message , owner })=>{
+      return program.methods.createJournalEntry(title,message).rpc();
+    },
+    onSuccess:(signature)=>{
+      transactionToast(signature);
+      accounts.refetch()
+    },
+    onError:(error)=>{
+      toast.error(`Error creating entry : ${error.message}`);
+    }
+  })
+ 
+
+  return {
+    program,
+    programId,
+    accounts,
+    getProgramAccount,
+    createEntry
+  }
+}
+
+export function useCrudProgramAccount({ account }: { account: PublicKey }) {
+  const { cluster } = useCluster()
+  const transactionToast = useTransactionToast()
+  const { program, accounts } = useCrudProgram()
+
+  const accountQuery = useQuery({
+    queryKey: ['crud', 'fetch', { cluster, account }],
+    queryFn: () => program.account.journalEntryState.fetch(account),
+  })
+  
+  const updateEntry = useMutation<string , Error,CreateEntryArgs>({
+    mutationKey : [`journalEntry` , `update` , {cluster}],
+    mutationFn :async({title , message  })=>{
+      return program.methods.updateJournalEntry(title,message).rpc();
+    },
+    onSuccess:(signature)=>{
+      transactionToast(signature);
+      accounts.refetch()
+    },
+    onError:(error)=>{
+      toast.error(`Error updating entry : ${error.message}`);
+    }
+  })
+  const deleteEntry = useMutation({
+    mutationKey : [`journalEntry` , `delete` , {cluster}],
+    mutationFn :(title:string)=>{
+      return program.methods.deleteJournalEntry(title).rpc();
+    },
+    onSuccess:(signature)=>{
+      transactionToast(signature);
+      accounts.refetch()
+    },
+    onError:(error)=>{
+      toast.error(`Error updating entry : ${error.message}`);
+    }
+  })
+  return {
+    accountQuery,
+    updateEntry,
+    deleteEntry
+  }
+}
