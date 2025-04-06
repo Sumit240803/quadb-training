f9e69b804d5265a0bbb5c45a9c80ef027c43f8aa
Sumit240803 <goyalsumit651@gmail.com>
2025-04-07 03:22:03 +0530

    Game Server - Play Cards Socket Completed

--- misc-projects/smart-contracts/card-game/gameLogic.js
+++ misc-projects/smart-contracts/card-game/gameLogic.js
@@ -1,3 +1,18 @@
-function playCard(card){
-    
+function result(points,player1 , player2,scores,roomId) {
+    const set1 = points[player1];
+    const set2 = points[player2];
+    const player1Damage = Math.max(0 , set1.attack - set2.defense);
+    const player2Damage = Math.max(0,set2.attack - set1.defense);
+    if(player1Damage > player2Damage){
+        scores[roomId][player1]+=player1Damage;
+        return scores;
+    }else if (player2Damage > player1Damage){
+        scores[roomId][player2]+=player2Damage;
+        return scores; 
+    }else{
+        scores[roomId][player1]+=player1Damage;
+        scores[roomId][player2]+=player2Damage;
+        return scores;
+    }
 }
+module.exports = {result};
--- misc-projects/smart-contracts/card-game/server/server.js
+++ misc-projects/smart-contracts/card-game/server/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const http = require("http");
 const {Server} = require("socket.io");
 const { generate } = require("./generateDeck");
+const { result } = require("../gameLogic");
+const { json } = require("stream/consumers");
 const app = express();
 
 app.use(cors());
@@ -15,16 +17,8 @@ const io = new Server(server,{
 })
 const roomDecks = {};
 const roomState = {};
-var card1={
-    rarity : "common",
-    attack : 45,
-    defense : 35
-}
-var card2={
-    rarity : "common",
-    attack : 55,
-    defense : 15
-}
+const points = {};
+const scores = {};
 io.on("connection",(socket)=>{
     console.log("Player Connected" , socket.id);
 
@@ -52,42 +46,51 @@ io.on("connection",(socket)=>{
         
         
     })
-    socket.on("attack", (roomId) => {
-        if (roomDecks[roomId] && roomDecks[roomId][socket.id] && roomState[roomId]?.turn === socket.id) {
-            const msg = `Player ${socket.id} attacked with ${card1.attack} points`;
-            socket.emit("action", msg);
-            socket.to(roomId).emit("opponent-action", msg);
-            console.log("Attacked");
-            const players = Object.keys(roomDecks[roomId]);
-            const newPlayer = players.find(p => p!==socket.id);
-            roomState[roomId].turn = newPlayer;
-            io.to(roomId).emit("turn-change",newPlayer);
-        }else{
-            socket.emit("error","Not your turn or Some other error")
-        }
-    });
-    
-    socket.on("defend", (roomId) => {
-        if (roomDecks[roomId] && roomDecks[roomId][socket.id] && roomState[roomId]?.turn === socket.id) {
-        const msg = `Player ${socket.id} defended with ${card2.defense} points`;
-        socket.emit("action", msg);
-        socket.to(roomId).emit("opponent-action", msg);
-        console.log("Defended");
-        const players = Object.keys(roomDecks[roomId]);
-        const newPlayer = players.find(p => p!==socket.id);
-        roomState[roomId].turn = newPlayer;
-        io.to(roomId).emit("turn-change",newPlayer);
-        }else{
-            socket.emit("error","Not your turn or Some other error")
-        }
-    });
-    
+  
 
     //Playing The Card
-    socket.on("play-card",({roomId,cardData})=>{
-        console.log(`Player with id ${socket.id} played ${cardData.name} with attribute ${cardData.value}`);
-        socket.to(roomId).emit("opponent-played", cardData.suit);
-    })
+    socket.on("play-card", ({ roomId, cardData, round }) => {
+        const playerId = socket.id;
+        const room = roomDecks[roomId];
+        if (room && room[playerId] && roomState[roomId]?.turn === playerId) {
+            const [player1, player2] = Object.keys(room);
+    
+            if (!points[roomId]) points[roomId] = {};
+            if (!points[roomId][round]) points[roomId][round] = {};
+    
+            const cardStats = {
+                attack: cardData.attack || 0,
+                defense: cardData.defense || 0
+            };
+            points[roomId][round][playerId] = cardStats;
+    
+            socket.to(roomId).emit("opponent-played", { playerId, cardData });
+    
+            const nextTurn = playerId === player1 ? player2 : player1;
+            roomState[roomId].turn = nextTurn;
+            io.to(roomId).emit("turn-change", nextTurn);
+    
+            console.log(`✅ Round ${round}: ${playerId} played card`, cardStats);
+            console.log("Points:", JSON.stringify(points));
+    
+            // Only evaluate result if both players have played
+            if (
+                points[roomId][round][player1] &&
+                points[roomId][round][player2]
+            ) {
+                if (!scores[roomId]) scores[roomId] = {};
+                if (!scores[roomId][player1]) scores[roomId][player1] = 0;
+                if (!scores[roomId][player2]) scores[roomId][player2] = 0;
+    
+                const score = result(points[roomId][round], player1, player2, scores, roomId);
+                console.log("✅ Round result calculated:", JSON.stringify(score));
+    
+                // Clean up if needed
+                delete points[roomId][round];
+            }
+        }
+    });
+    
     socket.on("game-result", ({ roomId, result }) => {
         console.log(`🏆 Game finished in ${roomId}`);
         socket.to(roomId).emit("game-result", result);
@@ -100,3 +103,34 @@ const PORT = 3001;
 server.listen(PORT,()=>{
     console.log(`Web Socket Running on port ${PORT}`)
 })
+
+/*  socket.on("attack", (roomId,card) => {
+        if (roomDecks[roomId] && roomDecks[roomId][socket.id] && roomState[roomId]?.turn === socket.id) {
+            const msg = `Player ${socket.id} attacked with ${card.attack} points`;
+            socket.emit("action", msg);
+            socket.to(roomId).emit("opponent-action", msg);
+            console.log("Attacked");
+            const players = Object.keys(roomDecks[roomId]);
+            const newPlayer = players.find(p => p!==socket.id);
+            roomState[roomId].turn = newPlayer;
+            io.to(roomId).emit("turn-change",newPlayer);
+        }else{
+            socket.emit("error","Not your turn or Some other error")
+        }
+    });
+    
+    socket.on("defend", (roomId,card) => {
+        if (roomDecks[roomId] && roomDecks[roomId][socket.id] && roomState[roomId]?.turn === socket.id) {
+        const msg = `Player ${socket.id} defended with ${card.defense} points`;
+        socket.emit("action", msg);
+        socket.to(roomId).emit("opponent-action", msg);
+        console.log("Defended");
+        const players = Object.keys(roomDecks[roomId]);
+        const newPlayer = players.find(p => p!==socket.id);
+        roomState[roomId].turn = newPlayer;
+        io.to(roomId).emit("turn-change",newPlayer);
+        }else{
+            socket.emit("error","Not your turn or Some other error")
+        }
+    });
+    */
